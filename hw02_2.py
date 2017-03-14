5845b1396254f546ab2e2c6378524405b539f127
chaumt <mtchau@me.com>
2017-03-13 23:02:37 -0700

    new test python file

---
+++ hw02_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+""" Homework 1 Lab2 #2
+"""
+
+while True:
+    name = raw_input("What's your name? ")
+    if not name.isalpha():
+	    print 'Please try again.'
+    else:
+        break
+
+while True:
+    number = raw_input('How tall are you in inches? ')
+    try:
+        height = float(number)
+    except ValueError:
+        print "'" + number + "'" + " is not a number! Please try again."
+    else:
+        break
+
+if height > 72:
+    print "%s, " % name + "you are %.1f feet which is tall." % (height/12)
+elif height > 60 and height <= 72:
+    print "%s, " % name + "you are %.1f feet which is medium." % (height/12)
+else:
+    print "%s, " % name + "you are %.1f feet which is short." % (height/12)
+
+
